c10bc5f6b742db431f101071b4cdf1749948192e
root <root@vultr.guest>
2021-09-16 10:35:19 +0700

    master

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/wapa';
+$config['base_url'] = 'https://whatsva.id';
 
 /*
 |--------------------------------------------------------------------------
@@ -52,7 +52,8 @@ $config['index_page'] = 'index.php';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'REQUEST_URI';
+$config['uri_protocol'] = "REQUEST_URI";
+
 
 /*
 |--------------------------------------------------------------------------
