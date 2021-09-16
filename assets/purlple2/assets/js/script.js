33640a9374c0eef51d4dfd79bc1e447b71d6a5b6
novaariyanto <bajangsampoerna@gmail.com>
2021-09-16 10:06:11 +0700

    master

---
+++ assets/purlple2/assets/js/script.js
@@ -0,0 +1,47 @@
+
+    var url = window.location.href;
+    console.log('develop by whatsva.com')
+    if(url.includes("/device/authqr/")){
+        var urla = window.location.pathname;
+        var arr = urla.split('/')
+        console.log(window.location.host)
+
+        console.log("change qr")
+        var timeleft = 15;
+        var downloadTimer = setInterval(function(){
+        timeleft--;
+        document.getElementById("countdowntimer").textContent = timeleft;
+        if(timeleft <= 0)
+            clearInterval(downloadTimer);
+            if(timeleft < 1){
+                window.location.reload();
+            }
+        },1000);
+        
+        setInterval(() => {
+            console.log("change qr")
+         
+
+            $.ajax({
+                url: url + "./../../../device/detail/"+arr[5],
+                type: "get",
+                dataType: 'json',
+                success: function (hasil) {
+                  // console.log(hasil)
+                  // Put the object into storage
+                  if (hasil.success) {
+                    if(hasil.data.instance_status === "connected"){
+                        window.location.href = "./../../device"
+                    }
+                    $('#qrcode').html("")
+                    $('#qrcode').qrcode({width: 200,height: 200,text: hasil.data.qrCode});
+                  } else {
+                    alert(hasil.message)
+                  }
+                }
+              })
+        }, 2000);
+
+
+    }
+   
