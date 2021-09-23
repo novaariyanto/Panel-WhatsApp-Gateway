6110e08633f6d8dd7a86d8836883f803370cc784
novaariyanto <bajangsampoerna@gmail.com>
2021-09-23 13:25:44 +0700

    remove index.php in url

--- application/config/config.php
+++ application/config/config.php
@@ -25,6 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $config['base_url'] = 'http://localhost/wapa';
 
+
 /*
 |--------------------------------------------------------------------------
 | Index File
